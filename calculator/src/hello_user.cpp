1833dfca952bd54b261ded7e519864d0f2cc9651
Kaiserroman <romantrud6162@gmail.com>
2022-09-23 13:10:24 +0300

    feat: add hello_user.cpp

---
+++ calculator/src/hello_user.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    string username;
+    cout << "What's your name?" << endl;
+    cin >> username;
+    cout << "Hello," << username << endl;;
+    system("PAUSE");
+    return 0;
+}
