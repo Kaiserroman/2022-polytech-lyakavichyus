68039d58c55abbb35b1f4aa7802105de7deeaee9
Kaiserroman <romantrud6162@gmail.com>
2022-10-20 22:44:12 +0300

    feat: add README.md, intro.cpp and CMakeLists.txt

---
+++ 06_pointers_references/intro/intro.cpp
@@ -0,0 +1,25 @@
+﻿// intro.cpp: определяет точку входа для приложения.
+//
+
+#include <cassert>
+#include <string>
+using namespace std;
+
+unsigned int length(const char* str)
+{
+    if (str == nullptr) {
+        return 0;
+    }
+    else 
+        return strlen(str);
+}
+
+int main()
+{
+    assert(length("a") == 1);
+    assert(length("ab") == 2);
+    assert(length("") == 0);
+    assert(length("hello world") == 11);
+    assert(length(nullptr) == 0);
+    return 0;
+}
