4e053230f4990897ea85ae4a384801c579d5a735
Kaiserroman <romantrud6162@gmail.com>
2022-10-28 12:31:56 +0300

    feat: add polindrome.cpp and CMakeLists.txt

---
+++ 06_pointers_references/palindrome/palindrome.cpp
@@ -0,0 +1,66 @@
+﻿// intro.cpp: определяет точку входа для приложения.
+//
+
+#include <cassert>
+using namespace std;
+
+unsigned int length(const char* str)
+{
+    if (str == nullptr || length(str) == 0) {
+        return 0;
+    }
+    else {
+        int size = 0;
+        while (*str++) size++;
+        return size;
+    }
+}
+
+bool palcheck(const char* str) {
+    if (str == nullptr) {
+        return false;
+    }
+    else {
+        if (length(str) == 1)
+            return true;
+        else {
+            if (length(str) % 2 == 0) {
+                int n = length(str);
+                for (int i = 0; i < (n / 2); i++) {
+                    if (str[n / 2 - 1 - i] == str[n / 2 + i]) {
+                        continue;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else {
+                int n = length(str);
+                for (int i = 0; i < (n / 2); i++) {
+                    if (str[n / 2 - i] == str[n / 2 + 1]) {
+                        continue;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    assert(palcheck("a") == true);
+    assert(palcheck("ab") == false);
+    assert(palcheck("aa") == true);
+    assert(palcheck("") == false);
+    assert(palcheck("abcdcba") == true);
+    assert(palcheck("abcddcba") == true);
+    assert(palcheck(nullptr) == false);
+    return 0;
+}
