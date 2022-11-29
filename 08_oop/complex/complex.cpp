e51a75d4713f4d481f9ed88ddd616e11e2e4ce6b
Kaiserroman <romantrud6162@gmail.com>
2022-11-29 22:42:08 +0300

    feat: add 8 task

---
+++ 08_oop/complex/complex.cpp
@@ -0,0 +1,18 @@
+﻿#include "complex.h"
+#include <cmath>
+#include "doctest.h"
+
+const Complex Complex::operator+(const Complex rv) const { //описываем оператор сложения
+	return Complex(r_part + rv.r_part, i_part + rv.i_part);
+}
+const Complex Complex::operator-(const Complex rv) const { //описываем оператор вычитания
+	return Complex(r_part - rv.r_part, i_part - rv.i_part);
+}
+
+const bool Complex::operator==(const Complex rv) const { //описываем оператор сравнения
+	return r_part == rv.r_part && i_part == rv.i_part;
+}
+
+const double Complex::abs() const {
+	return sqrt(r_part * r_part + i_part * i_part); //описываем оператор модуля
+}
---
+++ 08_oop/complex/complex.h
@@ -0,0 +1,10 @@
+﻿class Complex {//объявляем класс
+public:
+	int r_part; //объявляем целую часть
+	int i_part; //объявляем часть с мнимой единицей
+	Complex(int r, int i) : r_part(r), i_part(i) {}; //объявляем консруктор
+	const Complex operator+(const Complex rv) const; //объявляем оператор сложения
+	const Complex operator-(const Complex rv) const; //объявляем опреатор вычитания 
+	const bool operator==(const Complex rv) const; //объявляем оператор сравнения
+	const double abs() const; //объявляем оператор модуля
+};
