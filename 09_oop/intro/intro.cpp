c84da3e70929e03db51fa8beadc4a1f37d929f1c
Kaiserroman <romantrud6162@gmail.com>
2022-12-02 05:43:56 +0300

    upd: task 9 intro, add README.md

---
+++ 09_oop/intro/intro.cpp
@@ -0,0 +1,33 @@
+﻿// intro.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Person {
+    Person() { std::cout << "Person::ctor" << std::endl; } //конструктор
+    ~Person() { std::cout << "Person::dtor" << std::endl; } //деструктор
+    std::string name{};
+};
+
+struct Student : virtual public Person {
+    Student() { std::cout << "Student::ctor" << std::endl; } //конструктор
+    ~Student() { std::cout << "Student::dtor" << std::endl; } //деструктор
+    int score{};
+};
+
+struct Teacher : virtual public Person {
+    Teacher() { std::cout << "Teacher::ctor" << std::endl; } //конструктор
+    ~Teacher() { std::cout << "Teacher::dtor" << std::endl; } //деструктор
+};
+
+struct TA : Teacher, Student {};
+
+int main()
+{
+    TA ta;
+    ta.score = 5.0;
+    ta.name = "Da Sha";
+    return 0;
+}
+
